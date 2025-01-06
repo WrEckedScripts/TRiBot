63b18cea1d0c36b4e2d1ecef82cdea5fbc5b5569
WrEcked <unknown@mail.com>
2025-01-06 21:52:39 +0100

    [Motherlode] Dropped parent sequence for repair node

--- scripts/wrMotherlode/src/scripts/wrMotherlode/behaviours/motherlode/actions/RepairBrokenStrutNode.kt
+++ scripts/wrMotherlode/src/scripts/wrMotherlode/behaviours/motherlode/actions/RepairBrokenStrutNode.kt
@@ -4,7 +4,6 @@ import org.tribot.script.sdk.MyPlayer
 import org.tribot.script.sdk.Waiting
 import org.tribot.script.sdk.frameworks.behaviortree.IParentNode
 import org.tribot.script.sdk.frameworks.behaviortree.condition
-import org.tribot.script.sdk.frameworks.behaviortree.sequence
 import org.tribot.script.sdk.query.Query
 import org.tribot.script.sdk.walking.LocalWalking
 import scripts.utils.Logger
@@ -12,38 +11,36 @@ import scripts.utils.antiban.FatigueResolver
 import scripts.utils.failsafes.LastActionTracker
 import kotlin.jvm.optionals.getOrNull
 
-fun IParentNode.repairBrokenStrutNode(logger: Logger) = sequence {
-    condition {
-        val brokenStrut = Query.gameObjects()
-            .actionContains("Hammer")
-            .findBestInteractable()
-            .getOrNull()
+fun IParentNode.repairBrokenStrutNode(logger: Logger) = condition {
+    val brokenStrut = Query.gameObjects()
+        .actionContains("Hammer")
+        .findBestInteractable()
+        .getOrNull()
 
-        if (null == brokenStrut) {
-            logger.info("No broken struts, continue-ing!")
+    if (null == brokenStrut) {
+        logger.info("No broken struts, continue-ing!")
 
-            return@condition true
+        return@condition true
+    }
+
+    if (brokenStrut.tile.isOnMinimap || brokenStrut.tile.isVisible) {
+        Waiting.waitUntil(20_000, 5_000 + FatigueResolver.getMilliseconds()) {
+            brokenStrut.interact("Hammer")
+
+            LastActionTracker.track("click")
+
+            Waiting.wait(FatigueResolver.getMilliseconds())
+
+            Query.gameObjects()
+                .actionContains("Hammer")
+                .findBestInteractable()
+                .isEmpty
         }
+    } else {
+        Waiting.waitUntil(15_000, 2_000 + FatigueResolver.getMilliseconds()) {
+            LocalWalking.walkTo(brokenStrut.tile)
 
-        if (brokenStrut.tile.isOnMinimap || brokenStrut.tile.isVisible) {
-            Waiting.waitUntil(20_000, 5_000 + FatigueResolver.getMilliseconds()) {
-                brokenStrut.interact("Hammer")
-
-                LastActionTracker.track("click")
-
-                Waiting.wait(FatigueResolver.getMilliseconds())
-
-                Query.gameObjects()
-                    .actionContains("Hammer")
-                    .findBestInteractable()
-                    .isEmpty
-            }
-        } else {
-            Waiting.waitUntil(15_000, 2_000 + FatigueResolver.getMilliseconds()) {
-                LocalWalking.walkTo(brokenStrut.tile)
-
-                MyPlayer.getTile() != brokenStrut.tile
-            }
+            MyPlayer.getTile() != brokenStrut.tile
         }
     }
 }
